6c5a4bc3b23c7e64870d0f70af664bed8297cb51
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-18 16:58:56 +0100

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,26 @@
+import 'package:banking_app_ui/components/appbar.dart';
+import 'package:banking_app_ui/components/cards.dart';
+import 'package:banking_app_ui/components/recentTransactions.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        child: Column(
+          children: [
+            Appbar(),
+            CardsList(),
+            RecentTransactions(),
+          ],
+        ),
+      ),
+    );
+  }
+}
